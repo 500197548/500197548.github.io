cfd233454ec7cc85f2a1eaf3755486bb21aedc93
Sebastian Poquette <500197548@fvtc.edu>
2024-08-26 15:59:47 -0500

    first commit

---
+++ site.js
@@ -0,0 +1,26 @@
+// const pageHeader = document.querySelector('h1')
+// pageHeader.textContent = `Sebastian's Webpage`
+
+// const p = document.querySelector('footer > p')
+// p.textContent = 'Something Stupid'
+
+// const links = document.querySelectorAll('.active')
+// for (let i = 0; i < links.length; i++){
+//     links[i].style.color = 'Black'
+// }
+
+// document.body.style.backgroundColor = 'grey'
+// document.title = pageHeader.textContent
+
+// const nav = document.querySelector('nav')
+// const contact = nav.querySelector('#contact')
+// contact.style.textDecoration = 'underline'
+
+// function test(event) {
+//     //console.log(event.target.parentElement)
+//     console.log(event.target.closest('section'))
+// }
+
+// const p = document.querySelector('section p')
+// p.addEventListener('click', test)
+
