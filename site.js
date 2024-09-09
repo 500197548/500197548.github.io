1ea8156a1c4bd4814f25e363b505f93cf206bc24
Sebastian Poquette <500197548@fvtc.edu>
2024-09-09 15:36:16 -0500

    Added Welcome div

--- site.js
+++ site.js
@@ -24,3 +24,14 @@
 // const p = document.querySelector('section p')
 // p.addEventListener('click', test)
 
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+const welcome = document.querySelector('.welcome h2')
+
+if (isMorning){welcome.textContent='Good Morning!'}
+if (isAfternoon){welcome.textContent='Good Afternoon!'}
+if (isEvening){welcome.textContent='Good Evening!'}
