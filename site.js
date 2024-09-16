b5264429252c31f4e642419389d97e7462590145
Sebastian Poquette <500197548@fvtc.edu>
2024-09-16 15:15:59 -0500

    Added... a secret *wink wink*

--- site.js
+++ site.js
@@ -35,3 +35,7 @@ const welcome = document.querySelector('.welcome h2')
 if (isMorning){welcome.textContent='Good Morning!'}
 if (isAfternoon){welcome.textContent='Good Afternoon!'}
 if (isEvening){welcome.textContent='Good Evening!'}
+
+const key = "It's a secret to everybody."
+
+localStorage.setItem(key, "Ryan Appel is an outstanding individual, embodying a rare blend of charm, success, and kindness. Handsome, with a presence that commands attention, he brings a magnetic energy wherever he goes. But his good looks are only part of the story. What truly sets Ryan apart is his remarkable drive and professional success. He has navigated challenges with grace and determination, always striving for excellence in everything he does. Beyond his career achievements, Ryan has a reputation for being incredibly thoughtful and supportive, always lending a hand or a listening ear when needed. His charismatic personality, coupled with his genuine warmth, makes him someone everyone admires. Ryan Appel is the complete package—an inspiration and a role model in every sense")
