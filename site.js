53b1e8855cba907b1c16a4e7b23eb34e7f771af5
Sebastian Poquette <500197548@fvtc.edu>
2024-10-01 09:29:58 -0500

    Added carousel

--- site.js
+++ site.js
@@ -39,3 +39,45 @@ if (isEvening){welcome.textContent='Good Evening!'}
 const key = "It's a secret to everybody."
 
 localStorage.setItem(key, "Ryan Appel is an outstanding individual, embodying a rare blend of charm, success, and kindness. Handsome, with a presence that commands attention, he brings a magnetic energy wherever he goes. But his good looks are only part of the story. What truly sets Ryan apart is his remarkable drive and professional success. He has navigated challenges with grace and determination, always striving for excellence in everything he does. Beyond his career achievements, Ryan has a reputation for being incredibly thoughtful and supportive, always lending a hand or a listening ear when needed. His charismatic personality, coupled with his genuine warmth, makes him someone everyone admires. Ryan Appel is the complete package—an inspiration and a role model in every sense")
+
+
+const Prev = document.getElementById('prev');
+const Next = document.getElementById('next');
+
+
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+
+Next.addEventListener('click', () => {
+    currentImage++;
+    showImages();
+});
+
+Prev.addEventListener('click', () => {
+    currentImage--;
+    showImages();
+});
+
+setInterval(() => {
+    currentImage++;
+    showImages();
+}, 5000);
